e2109c8c9fa2de90688e176046563a540da3bfc0
MaksimkaAzarov <ma@devurai.com>
2026-05-02 09:24:42 +0300

    format on save

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -20,4 +21,14 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+  {
+    ...eslintPluginPrettierRecommended,
+    files: ['**/*.{ts,tsx}'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
+]);
